0cec89b4c68c3aa06e3ea49c0153838c2204ecd8
Francesc <elskater98@gmail.com>
2019-02-22 22:54:58 +0100

    Class PHOTO -> added new attributes and functionality

--- repository/admin.py
+++ repository/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Photo
+from .models import Photo,History_connections
 # Register your models here.
 admin.site.register(Photo)
+admin.site.register(History_connections)
--- repository/models.py
+++ repository/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Photo(models.Model):
-    name = models.CharField(max_length=200,help_text="Enter a name ")
-    resolution = models.CharField(help_text="Enter a name ")
+    name = models.CharField(max_length=200,help_text="Enter a name to the snap",null=False)
+    picture=models.ImageField(default='PICTURE_DEFAULT_NAME',help_text='Choose a picture')
 
+    RESOLUTION_STATUS = (('HD',('1280×720 píxels')), ('FHD','1920×1080 pixels'),
+                         ('QHD','2560*1440 pixels'), ('UHD','3840×2160 píxels'))
 
+    select_resolution=models.CharField(max_length=1, choices=RESOLUTION_STATUS, blank=False, default='FHD',
+                              help_text='Pick up the resolution')
+
+    RATING_CHOICES=((1, 'one'), (2, 'two'), (3, 'three'), (4, 'four'), (5, 'five'))
+
+    rating = models.PositiveSmallIntegerField('Rating (stars)', blank=False, default=3, choices=RATING_CHOICES)
+
+    author=models.ForeignKey(User,default=1,on_delete=models.SET_DEFAULT)
+
+class History_connections(models.Model):
+    date = models.DateTimeField()
