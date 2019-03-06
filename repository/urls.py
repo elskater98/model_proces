9dc9e7a377d05ee47285421bb9e6ceb4c93673cd
Francesc <elskater98@gmail.com>
2019-03-06 20:46:06 +0100

    URBAN PAGE

--- repository/urls.py
+++ repository/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$', views.homepage, name='homepage')
+    url(r'^$', views.homepage, name='homepage'),
+    url(r'^$', views.urban_page, name='urban'),
 ]
--- repository/views.py
+++ repository/views.py
@@ -6,3 +6,7 @@ from .models import Photo
 def homepage(request):
     photo = Photo.objects.all()
     return render(request,'homepage.html',context={'photo':photo})
+
+def urban_page(request):
+    photo = Photo.objects.all()
+    return render(request, 'urban.html', context={'photo': photo})
