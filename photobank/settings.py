95a7f953179a145bbc49c09167bdb19d220ee519
Francesc <elskater98@gmail.com>
2019-03-09 00:29:27 +0100

    SIGN UP : DONE

--- photobank/settings.py
+++ photobank/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'repository.apps.RepositoryConfig'
+    'repository.apps.RepositoryConfig',
+    'accounts.apps.AccountsConfig',
+
 ]
 
 MIDDLEWARE = [
