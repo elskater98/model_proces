c2c7e44378ba4d00e5869812da948bd7733e7cd2
Francesc <elskater98@gmail.com>
2019-03-16 15:29:13 +0100

    Orderbt date, new theme

--- repository/urls.py
+++ repository/urls.py
@@ -5,5 +5,7 @@ from django.urls import path
 urlpatterns=[
     path('', views.homepage, name='homepage'),
     path('abstract/', views.abstract, name='abstract'),
-    path('nature/', views.nature, name='nature')
+    path('nature/', views.nature, name='nature'),
+    path('minimal/',views.minimal, name='minimal'),
+
 ]
--- repository/views.py
+++ repository/views.py
@@ -3,19 +3,26 @@ from .models import *
 # Create your views here.
 
 def homepage(request):
-    photo = Photo.objects.all()
+    photo = Photo.objects.all().order_by('-date')
     theme= Theme.objects.all()
     author= Author.objects.all()
     return render(request,'homepage.html',context={'photo':photo,'theme':theme,'author':author})
 
 def abstract(request):
-    photo = Photo.objects.filter(select_theme__name__contains='abstract')
+    photo = Photo.objects.filter(select_theme__name__contains='abstract').order_by('-date')
     theme = Theme.objects.all()
     author = Author.objects.all()
     return render(request, 'abstract.html', context={'photo':photo, 'theme':theme, 'author':author})
 
 def nature(request):
-    photo = Photo.objects.filter(select_theme__name__contains='nature')
+    photo = Photo.objects.filter(select_theme__name__contains='nature').order_by('-date')
     theme = Theme.objects.all()
     author = Author.objects.all()
     return render(request, 'nature.html', context={'photo':photo, 'theme':theme, 'author':author})
+
+def minimal(request):
+    photo = Photo.objects.filter(select_theme__name__contains='minimal').order_by('-date')
+    theme = Theme.objects.all()
+    author = Author.objects.all()
+    return render(request, 'minimal.html', context={'photo':photo, 'theme':theme, 'author':author})
+
